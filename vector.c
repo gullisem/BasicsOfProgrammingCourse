0d9e14a3298b1b434ef834e2a02776fb8cadb32e
gullisem <nikzhidkov.gmail.com>
2023-04-09 19:06:54 +0300

    first commit

---
+++ vector.c
@@ -0,0 +1,7 @@
+#include "vector.h"
+
+#include <stdio.h>
+
+void hello(void) {
+    printf("Hello, World!\n");
+}
---
+++ vector.h
@@ -0,0 +1,6 @@
+#ifndef GITPRO_VECTOR_H
+#define GITPRO_VECTOR_H
+
+void hello(void);
+
+#endif //GITPRO_VECTOR_H
