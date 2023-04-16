af9958285110727919a4791f0067c2b1df7e90a2
gullisem <nikzhidkov.gmail.com>
2023-04-16 20:37:41 +0300

    append push / pop functions

--- main.c
+++ main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <stdint.h>
 #include "libs/data_structures/vector/vector.h"
+#define SIZE_MAX 128
+
+void test() {
+    test_pushBack_emptyVector();
+    test_pushBack_fullVector();
+    test_popBack_notEmptyVector();
+}
+
 
 int main() {
+
+    vector v = createVector(0);
+    test();
 
     return 0;
 }
